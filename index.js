7fc08c7d06d0680d376b99cd6524385583009e5c
Michal Piechowiak <misiek.piechowiak@gmail.com>
2019-03-16 21:28:36 +0100

    npm install

--- index.js
+++ index.js
@@ -38,7 +38,8 @@ const handler = async event => {
   } https://${accessToken}@github.com/${PRBranchInfo.owner}/${
     PRBranchInfo.repo
   }.git ${repoCloneDir}`;
-  const installDepsCmd = `npm`;
+  // use npm and not yarn ... because it doesn't handle workspaces ;)
+  const installDepsCmd = `npm install`;
   const runFormatCmd = `npm run format`;
   const stageFilesCmd = `git add .`;
   const commitFilesCmd = `git commit --author="pieh-peril-test<misiek.piechowiak@gmail.com>" -m "chore: format"`;
