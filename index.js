0b0c82bdd9a47505f21a014d1814e9b39f8e6f5e
Michal Piechowiak <misiek.piechowiak@gmail.com>
2019-03-16 22:54:10 +0100

    disable workspaces

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ require("dotenv").config();
 const accessToken = process.env.GITHUB_ACCESS_TOKEN;
 const user = `pieh`;
 
-const pExec = (command, args = {}) =>
+const pExec = (command, execArgs = {}, log = command) =>
   new Promise((resolve, reject) => {
-    console.log(`$ ${command}`);
-    childProcess.exec(command, args, (err, stdout, stderr) => {
+    console.log(`$ ${log}`);
+    childProcess.exec(command, execArgs, (err, stdout, stderr) => {
       console.error(stderr);
       console.log(stdout);
       if (err) {
@@ -20,6 +20,25 @@ const pExec = (command, args = {}) =>
       resolve();
     });
   });
+
+const disableWorkspaces = async repoCloneDir => {
+  console.log("> Disabling workspaces");
+  const packageJsonPath = path.join(repoCloneDir, "package.json");
+  const packageJsonString = await fs.readFile(packageJsonPath, "utf-8");
+  const packageJson = JSON.parse(packageJsonString);
+
+  delete packageJson.workspaces;
+
+  console.log(JSON.stringify(packageJson));
+
+  await fs.outputFile(packageJsonPath, JSON.stringify(packageJson));
+
+  return async () => {
+    console.log("> Re-enabling workspaces");
+    await fs.outputFile(packageJsonPath, packageJsonString);
+  };
+  // const package require
+};
 
 const handler = async event => {
   const repoCloneDir = path.join(process.cwd(), `_pr_clone_${event.pr}`);
@@ -44,13 +63,19 @@ const handler = async event => {
     const runFormatCmd = `yarn format`;
     const stageFilesCmd = `git add .`;
     const gitConfigEmailCmd = `git config user.email "misiek.piechowiak@gmail.com"`;
-    const gitConfigNameCmd = `git config user.name "pieh-peril-test`;
+    const gitConfigNameCmd = `git config user.name "pieh-peril-test"`;
     const commitFilesCmd = `git commit --author="pieh-peril-test<misiek.piechowiak@gmail.com>" -m "chore: format"`;
     const pushCmd = `git push origin ${PRBranchInfo.ref}`;
 
     await pExec(cloneCmd, execArgs);
     execArgs.cwd = repoCloneDir;
+
+    const restorePackageJson = await disableWorkspaces(repoCloneDir);
+
     await pExec(installDepsCmd, execArgs);
+
+    await restorePackageJson();
+
     try {
       await pExec(runFormatCmd, execArgs);
     } finally {
