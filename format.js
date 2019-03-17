860613794ea370ecb113f154f699eb24d3852b4e
Michal Piechowiak <misiek.piechowiak@gmail.com>
2019-03-17 01:02:54 +0100

    dynamiccaly get branch

---
+++ format.js
@@ -0,0 +1,108 @@
+const childProcess = require("child_process");
+const path = require("path");
+const fs = require(`fs-extra`);
+
+const accessToken = process.env.GITHUB_ACCESS_TOKEN;
+
+const pExec = (command, execArgs = {}, log = command) =>
+  new Promise((resolve, reject) => {
+    console.log(`$ ${log}`);
+    childProcess.exec(command, execArgs, (err, stdout, stderr) => {
+      console.error(stderr);
+      console.log(stdout);
+      if (err) {
+        reject(err);
+      }
+
+      resolve();
+    });
+  });
+
+const disableWorkspaces = async repoCloneDir => {
+  console.log("> Disabling workspaces");
+  const packageJsonPath = path.join(repoCloneDir, "package.json");
+  const packageJsonString = await fs.readFile(packageJsonPath, "utf-8");
+  const packageJson = JSON.parse(packageJsonString);
+
+  delete packageJson.workspaces;
+
+  console.log(JSON.stringify(packageJson));
+
+  await fs.outputFile(packageJsonPath, JSON.stringify(packageJson));
+
+  return async () => {
+    console.log("> Re-enabling workspaces");
+    await fs.outputFile(packageJsonPath, packageJsonString);
+  };
+  // const package require
+};
+
+const owner = "pieh";
+const repo = "gatsby";
+
+const getPRBranchInfo = async context => {
+  const { octokit, args } = context;
+  const result = await octokit.pulls.get({
+    owner,
+    repo,
+    number: args.pr
+  });
+
+  return {
+    ref: result.data.head.ref,
+    owner: result.data.head.repo.owner.login,
+    repo: result.data.head.repo.name
+  };
+};
+
+const handler = async context => {
+  const { args } = context;
+  const repoCloneDir = path.join(process.cwd(), `_pr_clone_${args.pr}`);
+
+  const PRBranchInfo = await getPRBranchInfo(context);
+
+  const execArgs = {
+    stdio: `inherit`
+  };
+
+  try {
+    const cloneCmd = `git clone --single-branch --branch ${
+      PRBranchInfo.ref
+    } https://${accessToken}@github.com/${PRBranchInfo.owner}/${
+      PRBranchInfo.repo
+    }.git ${repoCloneDir}`;
+    const installDepsCmd = `yarn --production=false`;
+    const runFormatCmd = `yarn format`;
+    const stageFilesCmd = `git add .`;
+    const unstageYarnLockCmd = `git reset HEAD yarn.lock`;
+    const gitConfigEmailCmd = `git config user.email "misiek.piechowiak@gmail.com"`;
+    const gitConfigNameCmd = `git config user.name "GatsbyJS Bot"`;
+    const commitFilesCmd = `git commit --author="GatsbyJS Bot<misiek.piechowiak@gmail.com>" --no-verify -m "chore: format"`;
+    const pushCmd = `git push origin ${PRBranchInfo.ref}`;
+
+    await pExec(cloneCmd, execArgs);
+    execArgs.cwd = repoCloneDir;
+
+    const restorePackageJson = await disableWorkspaces(repoCloneDir);
+
+    await pExec(installDepsCmd, execArgs);
+
+    await restorePackageJson();
+
+    try {
+      await pExec(runFormatCmd, execArgs);
+    } catch {
+    } finally {
+      await pExec(gitConfigEmailCmd, execArgs);
+      await pExec(gitConfigNameCmd, execArgs);
+      await pExec(stageFilesCmd, execArgs);
+      await pExec(unstageYarnLockCmd, execArgs);
+      await pExec(commitFilesCmd, execArgs);
+      await pExec(pushCmd, execArgs);
+    }
+  } finally {
+    await fs.removeSync(repoCloneDir);
+  }
+};
+
+module.exports = handler;
--- index.js
+++ index.js
@@ -1,104 +1,21 @@
-const childProcess = require("child_process");
-const path = require("path");
 const express = require(`express`);
-const fs = require(`fs-extra`);
+const Octokit = require("@octokit/rest");
+const format = require(`./format`);
 require("dotenv").config();
 
-const accessToken = process.env.GITHUB_ACCESS_TOKEN;
-const user = `pieh`;
-
-const pExec = (command, execArgs = {}, log = command) =>
-  new Promise((resolve, reject) => {
-    console.log(`$ ${log}`);
-    childProcess.exec(command, execArgs, (err, stdout, stderr) => {
-      console.error(stderr);
-      console.log(stdout);
-      if (err) {
-        reject(err);
-      }
-
-      resolve();
-    });
-  });
-
-const disableWorkspaces = async repoCloneDir => {
-  console.log("> Disabling workspaces");
-  const packageJsonPath = path.join(repoCloneDir, "package.json");
-  const packageJsonString = await fs.readFile(packageJsonPath, "utf-8");
-  const packageJson = JSON.parse(packageJsonString);
-
-  delete packageJson.workspaces;
-
-  console.log(JSON.stringify(packageJson));
-
-  await fs.outputFile(packageJsonPath, JSON.stringify(packageJson));
-
-  return async () => {
-    console.log("> Re-enabling workspaces");
-    await fs.outputFile(packageJsonPath, packageJsonString);
-  };
-  // const package require
-};
-
-const handler = async event => {
-  const repoCloneDir = path.join(process.cwd(), `_pr_clone_${event.pr}`);
-
-  const PRBranchInfo = {
-    ref: `peril-format-test`,
-    owner: `pieh`,
-    repo: `gatsby`
-  };
-
-  const execArgs = {
-    stdio: `inherit`
-  };
-
-  try {
-    const cloneCmd = `git clone --single-branch --branch ${
-      PRBranchInfo.ref
-    } https://${accessToken}@github.com/${PRBranchInfo.owner}/${
-      PRBranchInfo.repo
-    }.git ${repoCloneDir}`;
-    const installDepsCmd = `yarn --production=false`;
-    const runFormatCmd = `yarn format`;
-    const stageFilesCmd = `git add .`;
-    const unstageYarnLockCmd = `git reset HEAD yarn.lock`;
-    const gitConfigEmailCmd = `git config user.email "misiek.piechowiak@gmail.com"`;
-    const gitConfigNameCmd = `git config user.name "pieh-peril-test"`;
-    const commitFilesCmd = `git commit --author="pieh-peril-test<misiek.piechowiak@gmail.com>" --no-verify -m "chore: format"`;
-    const pushCmd = `git push origin ${PRBranchInfo.ref}`;
-
-    await pExec(cloneCmd, execArgs);
-    execArgs.cwd = repoCloneDir;
-
-    const restorePackageJson = await disableWorkspaces(repoCloneDir);
-
-    await pExec(installDepsCmd, execArgs);
-
-    await restorePackageJson();
-
-    try {
-      await pExec(runFormatCmd, execArgs);
-    } catch {
-    } finally {
-      await pExec(gitConfigEmailCmd, execArgs);
-      await pExec(gitConfigNameCmd, execArgs);
-      await pExec(stageFilesCmd, execArgs);
-      await pExec(unstageYarnLockCmd, execArgs);
-      await pExec(commitFilesCmd, execArgs);
-      await pExec(pushCmd, execArgs);
-    }
-  } finally {
-    await fs.removeSync(repoCloneDir);
-  }
-};
+const octokit = new Octokit({
+  auth: `token ${process.env.GITHUB_ACCESS_TOKEN}`
+});
 
 const app = express();
 
 app.get(`/format`, (req, res) => {
   if (req && req.query && req.query.pr) {
     console.log(req.query);
-    handler(req.query);
+    format({
+      args: req.query,
+      octokit
+    });
     res.send("running");
     return;
   }
@@ -106,6 +23,6 @@ app.get(`/format`, (req, res) => {
   res.send("missing stuff");
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, async () => {
   console.log("Server started");
 });
