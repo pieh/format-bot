7aef4227f56d304ec0a319898ec7dc58b31ef64e
Michal Piechowiak <misiek.piechowiak@gmail.com>
2019-03-17 03:27:37 +0100

    mark errors

--- utils.js
+++ utils.js
@@ -4,7 +4,11 @@ exports.pExec = (command, execArgs = {}, log = command) =>
   new Promise((resolve, reject) => {
     console.log(`$ ${log}`);
     childProcess.exec(command, execArgs, (err, stdout, stderr) => {
-      console.error(stderr);
+      if (stderr) {
+        console.log(` - ERR START`);
+        console.error(stderr);
+        console.log(` - ERR END`);
+      }
       console.log(stdout);
       if (err) {
         reject(err);
